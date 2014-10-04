8fe0a623617cfb74cae985471cb0a7deb07b8c41
Nikolaj Settnes <nsettnes@gmail.com>
2014-10-04 12:20:53 +0200

    works now.

--- src/kochpack/Logic.java
+++ src/kochpack/Logic.java
@@ -34,22 +34,12 @@ public class Logic
 			float secX = l.x1 + (deltaX/3);
 			float secY = l.y1 + (deltaY/3);
 			
-			double thirX =  ((l.x1 + l.x2)/2) + Math.sqrt(3) * (l.y1 - l.y2)/6;
-			double thirY =  ((l.y1 + l.y2)/2) + Math.sqrt(3) * (l.x1 - l.x2)/6;
-//			float midX = (l.x1 + l.x2) * 0.5f;
-//			float midY = (l.y1 + l.y2) * 0.5f;
-//			float thirX = l.x2 - midX;
-//			float thirY = l.y2 - midY;
-//			float temp = thirX;
-//			thirX = midX - thirY*(2/3);
-//			thirY = midY*(2/3) + temp;
+			double thirX =  ((l.x1 + l.x2)/2) + Math.sqrt(3) * (l.y1 - l.y2)/6; //this here be the magic
+			double thirY =  ((l.y1 + l.y2)/2) + Math.sqrt(3) * (l.x2 - l.x1)/6; //this here be the magic
 			
 			int fourthX = (int) (l.x1 + 2*(deltaX/3));
 			int fourthY = (int) (l.y1 + 2*(deltaY/3));
-			
-			//Line line = new Line(midtX,midtY,l.x1,l.y1);
-			//lubi.add(line);
-			
+
 			lubi.add(new Line(l.x1, l.y1, (int)secX, (int)secY));
 			lubi.add(new Line((int)secX, (int)secY, (int)thirX, (int)thirY));
 			lubi.add(new Line((int)thirX, (int)thirY, fourthX, fourthY));
--- src/kochpack/Main.java
+++ src/kochpack/Main.java
@@ -6,7 +6,7 @@ public class Main
 	{
 		Line[] lines = new Line[]
 			{
-				new Line(0,250,500,250)
+				new Line(500,250,0,250)
 			};
 		View.Instance().DrawLines(lines, false);
 		
